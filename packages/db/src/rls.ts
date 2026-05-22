14b38417ebb94fd3cde72da91969a949204c8f67
AkShAdIt <akshadit1976@gmail.com>
2026-05-23 02:30:23 +0530

    Remove mock database and cleanly transition to persistent PostgreSQL Prisma client

--- packages/db/src/rls.ts
+++ packages/db/src/rls.ts
@@ -1,17 +1,9 @@
-import { prisma, mockRlsContext } from "./client.js";
+import { prisma } from "./client.js";
 
 export async function withRls<T>(
   user: { id: string; orgId: string },
   fn: (tx: any) => Promise<T>,
 ): Promise<T> {
-  if (prisma.isDbReachable === false) {
-    return mockRlsContext.run({ currentUserId: user.id, currentOrgId: user.orgId }, async () => {
-      return await prisma.$transaction(async (tx: any) => {
-        return await fn(tx);
-      });
-    });
-  }
-
   return await prisma.$transaction(async (tx: any) => {
     await tx.$executeRawUnsafe(`SET LOCAL app.current_user_id = '${user.id}'`);
     await tx.$executeRawUnsafe(`SET LOCAL app.current_org_id = '${user.orgId}'`);
